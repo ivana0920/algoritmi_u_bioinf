21ad490dc73433c22082eeb28f95d8fddedd7d12
ivana0920 <81446395+ivana0920@users.noreply.github.com>
2021-03-31 20:29:28 +0200

    Add files via upload

---
+++ BA1C.py
@@ -0,0 +1,19 @@
+#Find a Profile-most Probable k-mer in a String
+#http://rosalind.info/problems/ba1c/
+
+def comp(text):
+    pattern=""
+    for i in range(0,len(text)):
+        if text[i]== "A":
+            pattern = "T" + pattern
+        elif text[i]== "C":
+            pattern = "G" + pattern
+        elif text[i]=="G":
+            pattern = "C" + pattern
+        elif text[i]== "T":
+            pattern = "A" + pattern
+        else:
+            return "Greška pri unosu"
+    return pattern
+
+print(comp('AAAACCCGGT'))
