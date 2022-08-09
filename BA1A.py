45ce9cb8fa3597d912eb2a39e6a2c8e9983b9616
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-09 20:59:40 +0200

    Update BA1A.py

--- BA1A.py
+++ BA1A.py
@@ -2,15 +2,11 @@
 # http://rosalind.info/problems/ba1a/
 # Compute the Number of Times a Pattern Appears in a Text
 
-def kmer(text, i, k):
-    return text[i:(i+k)]
+def PatternCount(text,pattern):
+  count=0
+  for i in range(0,len(text)-len(pattern)+1):
+    if text[i:i+len(pattern)]==pattern:
+      count+=1
+  return count
 
-def patterncount(text, pattern):
-    count = 0
-    np = len(pattern)
-    for i in range(0, len(text) - np + 1):
-        if kmer(text, i, np) == pattern:
-            count = count + 1
-    return count
-
-print(patterncount('GCGCG','GCG'))
+print(PatternCount("GCGCG","GCG"))
