96bf5fab1dfa5f845c0906c0c8b0bb05a6f408e5
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-10 23:34:41 +0200

    Update BA1D.py

--- BA1D.py
+++ BA1D.py
@@ -2,14 +2,14 @@
 # http://rosalind.info/problems/ba1d/
 # Find All Occurrences of a Pattern in a String
 
-def kmer(text, i, k):
-    return text[i:(i+k)]
+def kmer(text,i,k):
+  return text[i:(i+k)]
 
 def possition(pattern,text):
-    niz=[]
-    for i in range(0,len(text)-len(pattern)-1):
-        if(kmer(text,i,len(pattern))==pattern):
-            niz.append(i)
-    return niz
+  niz=[]
+  for i in range(0,len(text)-len(pattern)+1):
+    if(kmer(text,i,len(pattern))==pattern):
+      niz.append(i)
+  return niz
 
 print(possition("ATAT","GATATATGCATATACTT"))
