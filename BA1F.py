3c4bd689e2b9b40aad8fb9dde75e995a38ec8092
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-12 00:04:38 +0200

    Update BA1F.py

--- BA1F.py
+++ BA1F.py
@@ -2,28 +2,24 @@
 # Find a Position in a Genome Minimizing the Skew
 # https://rosalind.info/problems/ba1f/
 
-def difference(genome,l):
-  brC=0
-  brG=0
-  for i in range (0,l+1):
-    if (genome[i]=="C"):
-      brC+=1
-    if (genome[i]=="G"):
-      brG+=1
-  return (brG-brC)
-
 def skew(genome):
-  rez=dict()
-  rez[0]=0
-  for i in range (0,len(genome)):
-    if (i==0):
-      if (genome[i]=="C"):
-        rez[0]=-1
-      if (genome[i]=="G"):
-        rez[0]=1
+  rez=[0]*len(genome)
+  for i in range(0,len(genome)-1):
+    if genome[i]=="G":
+      rez[i+1]=rez[i]+1
+    elif genome[i]=="C":
+      rez[i+1]=rez[i]-1
     else:
-      rez[i]=difference(genome,i)
-  return [k for k in range (0,len(rez)) if rez[k]==min(rez.values())]
+      rez[i+1]=rez[i]
+  return rez
 
+def minskew(genome):
+  rez=list()
+  s=skew(genome)
+  minimum=min(s)
+  for i in range(0,len(s)):
+    if s[i]==minimum:
+      rez.append(i)
+  return rez
 
 print(skew("CCTATCGGTGGATTAGCATGTCCCTGTACGTTTCGCCGCGAACTAGTTCACACGGCTTGATGGCAAATGGTTTTTCCGGCGACCGTAATCGTCCACCGAG"))
