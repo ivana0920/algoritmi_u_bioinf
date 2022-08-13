6fca6465f6729b181a6204e30a4511a543d8a010
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-13 22:42:46 +0200

    Update BA1K.py

--- BA1K.py
+++ BA1K.py
@@ -2,3 +2,30 @@
 # Generate the Frequency Array of a String
 # https://rosalind.info/problems/ba1k/
 
+def SimbolToNumber(symbol):
+  if symbol=="A":
+    return 0
+  elif symbol=="C":
+    return 1
+  elif symbol=="G":
+    return 2
+  else:
+    return 3  
+
+def PatternToNumber(pattern):
+  if pattern=="":
+    return 0
+  symbol=pattern[-1]
+  prefix=pattern[:-1]
+  return 4*PatternToNumber(prefix)+SimbolToNumber(symbol)
+
+def FrequencyArray(text,k):
+  FArray=[0]*(4**k)
+  for i in range(0,len(text)-k+1):
+    pattern=text[i:i+k]
+    j=PatternToNumber(pattern)
+    FArray[j]=FArray[j]+1
+  return FArray  
+
+
+print(FrequencyArray("ACGCGGCTCTGAAA",2))
