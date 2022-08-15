326ae78ec114e34ecc9924bf1d71b3875d969534
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-16 00:14:05 +0200

    Update BA2F.py

--- BA2F.py
+++ BA2F.py
@@ -2,4 +2,30 @@
 # Implement RandomizedMotifSearch
 # https://rosalind.info/problems/ba2f/
 
+import random
 
+def HammingDistance(p,q):
+  br=0
+  for i in range(0,len(p)):
+    if p[i]!=q[i]:
+      br+=1
+  return br
+
+
+x="""
+8 5
+CGCCCCTCTCGGGGGTGTTCAGTAAACGGCCA
+GGGCGAGGTATGTGTAAGTGCCAAGGTGCCAG
+TAGTACCGAGACCGAAAGAAGTATACAGGCGT
+TAGATCAAGTTTCAGGTGCACGTCGGTGAACC
+AATCCACCAGCTCCACGTGCAATGTTGGCCTA
+"""
+inlines=x.split()
+k=int(inlines[0])
+t=int(inlines[0])
+dna=list()
+for i in range(2,len(inlines)):
+  dna.append(inlines[i])
+rez=RandomizedMotifSearch2(dna,k,t)
+for j in range(0,len(rez)):
+  print(rez[j])
