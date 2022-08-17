5aa4e5d3fd8cfeb9d83fb3e8f03cd3340415723c
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-17 13:23:08 +0200

    Update BA3A.py

--- BA3A.py
+++ BA3A.py
@@ -1,3 +1,20 @@
 # BA3A
 # Generate the k-mer Composition of a String
 # https://rosalind.info/problems/ba3a/
+
+def StringCompositionProblem(k,text):
+  rez=list()
+  for i in range(0,len(text)-k+1):
+    rez.append(text[i:i+k])
+  return sorted(rez) 
+
+
+x="""
+5
+CAATCCAAC
+"""
+inlines=x.split()
+k=int(inlines[0])
+text=inlines[1]
+for kmer in StringCompositionProblem(k,text):
+  print(kmer)
