3a9ebd770d19934daa65e1c234e9fc4616d398ae
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-17 20:50:26 +0200

    Update BA3C.py

--- BA3C.py
+++ BA3C.py
@@ -1,2 +1,28 @@
 # BA3C
-# 
+# Construct the Overlap Graph of a Collection of k-mers
+# https://rosalind.info/problems/ba3c/
+
+def Prefix(pattern):
+  return pattern[:len(pattern)-1]
+
+def Sufix(pattern):
+  return pattern[1:]
+
+def Overlap(patterns):
+  rez=list()
+  for pattern1 in patterns:
+    for pattern2 in patterns:
+      if Sufix(pattern1)==Prefix(pattern2):
+        rez.append(pattern1+"->"+pattern2)
+  return rez
+
+x="""
+ATGCG
+GCATG
+CATGC
+AGGCA
+GGCAT
+"""
+patterns=x.split()
+for pattern in Overlap(patterns):
+  print(pattern)
