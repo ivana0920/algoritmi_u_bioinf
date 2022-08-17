962b70b9e8bf4d14247f611debcb28caa1d7670b
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-17 15:42:28 +0200

    Update BA3B.py

--- BA3B.py
+++ BA3B.py
@@ -2,3 +2,19 @@
 # Reconstruct a String from its Genome Path
 # https://rosalind.info/problems/ba3b/
 
+def StringSpelledGenomePathProblem(kmers):
+  rez=kmers[0]
+  for i in range(0,len(kmers)):
+    rez+=kmers[i][-1]
+  return rez
+
+
+x="""
+ACCGA
+CCGAA
+CGAAG
+GAAGC
+AAGCT
+"""
+kmers=x.split()
+print(StringSpelledGenomePathProblem(kmers))
