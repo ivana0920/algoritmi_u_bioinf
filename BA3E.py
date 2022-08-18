2d4f50191d900eea26b831c62c5f1fb15c03b296
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-18 15:24:51 +0200

    Update BA3E.py

--- BA3E.py
+++ BA3E.py
@@ -2,3 +2,33 @@
 # Construct the De Bruijn Graph of a Collection of k-mers
 # https://rosalind.info/problems/ba3e/
 
+def Prefix(kmer):
+  return kmer[:len(kmer)-1]
+
+def Sufix(kmer):
+  return kmer[1:]
+
+def DeBruijn(patterns):
+  rez=dict()
+  patterns=sorted(patterns)
+  for pattern in patterns:
+    try:
+      rez[Prefix(pattern)].append(Sufix(pattern))
+    except KeyError:
+      rez[Prefix(pattern)]=[Sufix(pattern)]
+  return rez
+
+
+x="""
+GAGG
+CAGG
+GGGG
+GGGA
+CAGG
+AGGG
+GGAG
+"""
+patterns=x.split()
+rez=DeBruijn(patterns)
+for x in rez.keys():
+  print(x+" -> "+",".join(rez[x]))
