c9a99cd973bd4790fd38d179d0e7c18ffac307a9
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-21 11:38:01 +0200

    Update BA4D.py

--- BA4D.py
+++ BA4D.py
@@ -1,3 +1,25 @@
 # BA4D
-#
-#
+# Compute the Number of Peptides of Given Total Mass
+# https://rosalind.info/problems/ba4d/
+
+tablica=[57, 71, 87, 97, 99, 101, 103, 113, 114, 115, 128, 129, 131, 137, 147, 156, 163, 186]
+
+def RekMasa(masa,rez):
+  if masa in rez:
+    return rez[masa]
+  if masa<0:
+    return 0
+  if masa==0:
+      return 1
+  
+  rez[masa]=0
+  for x in tablica:
+    rez[masa]+=RekMasa(masa-x,rez)        
+  return rez[masa]
+
+def CountingPeptides(masa):
+  rez=dict()
+  return RekMasa(masa,rez)
+
+
+print(CountingPeptides(1024))
