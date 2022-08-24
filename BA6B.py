372e639869622d8c529bd2aff9b92203948a8b9b
imargaretic <81446395+ivana0920@users.noreply.github.com>
2022-08-25 00:37:01 +0200

    Update BA6B.py

--- BA6B.py
+++ BA6B.py
@@ -2,3 +2,20 @@
 # Compute the Number of Breakpoints in a Permutation
 # https://rosalind.info/problems/ba6b/
 
+ef Breakpoints(P):
+  print(P)
+  rez=0
+  for i in range(0,len(P)-1):
+    if P[i+1]-P[i]!=1:
+      rez+=1
+  return rez
+
+
+x="(+3 +4 +5 -12 -8 -7 -6 +1 +2 +10 +9 -11 +13 +14)"
+tmp=x[1:len(x)-1].split()
+P=list()
+P.append(0)
+for x in tmp:
+  P.append(int(x))
+P.append(len(P))
+print(Breakpoints(P))
